cb9e45299885fe5270c7754a396bec48abb05523
elias <eliastruman@users.noreply.github.com>
2024-09-23 19:59:08 +0300

    Обновление settings.py: заменены дефолтные значения. Кастомизация .env для безопасности, старые данные не актуальны.

--- config/settings.py
+++ config/settings.py
@@ -7,29 +7,19 @@ load_dotenv()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-BOT_NAME = os.getenv("BOT_NAME", "bot")
+BOT_NAME = os.getenv("BOT_NAME")
 
-# Попытка получить данные для PostgreSQL из переменных окружения
-POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", None)
-POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
-POSTGRES_DB = os.getenv("POSTGRES_DB", BOT_NAME)
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
 
-# Используем PostgreSQL как основное подключение, если переменные заданы
-if POSTGRES_USER and POSTGRES_DB:
-    if POSTGRES_PASSWORD:
-        DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
-    else:
-        DATABASE_URL = f"postgresql://{POSTGRES_USER}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
-else:
-    # Если данные для PostgreSQL не заданы, используем SQLite как резервный вариант
-    DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, f'database/{BOT_NAME}.db')}"
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
-# Настройки Redis
-REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
-REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
-REDIS_DB = int(os.getenv("REDIS_DB", 0))
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_PORT = int(os.getenv("REDIS_PORT"))
+REDIS_DB = int(os.getenv("REDIS_DB"))
 
 REDIS_SETTINGS = {
     "host": REDIS_HOST,
@@ -37,6 +27,5 @@ REDIS_SETTINGS = {
     "db": REDIS_DB,
 }
 
-# Настройки логирования
-LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
-LOG_LEVEL = getattr(logging, LOG_LEVEL, logging.INFO)
+LOG_LEVEL = os.getenv("LOG_LEVEL")
+LOG_LEVEL = getattr(logging, LOG_LEVEL)
